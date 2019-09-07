dbfcb7d1103be220628590c2771ad29cd86acfdb
yangqb <yangqb@ruyi.ai>
2019-09-07 18:33:00 +0800

    add docs

--- src/main/java/com/alphathur/excel/ExcelTitle.java
+++ src/main/java/com/alphathur/excel/ExcelTitle.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * excel标题头注解，当Java属性没有使用此注解，则默认使用Java属性作为标题。
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ExcelTitle {
